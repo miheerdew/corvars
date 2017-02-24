00c4b84a4425a1bceaa62d0df0f29e0c24b786e6
jpalowitch <johnpalowitch@gmail.com>
2017-02-24 01:48:02 -0500

    Deleted some files, added new makeVars code

--- makeVars.R
+++ makeVars.R
@@ -5,44 +5,17 @@ makeVars <- function(Uis, M_A){
   n1 = ncol(Uis)
   
   r1s = M_A %*% t(Uis)
-  k = length(r1s)
+  k = nrow(r1s)
   
-  Uis = Uis*sqrt(n1-1)
-  W = colMeans(M_A)*sqrt(n1-1)
-  Y = (t(r1s) %*% M_A^2)*(n1-1)/k
-  rA = mean(r1s)
+  Uis = Uis*sqrt(n1)
+  M_A = M_A*sqrt(n1)
+  W = t(array(colMeans(M_A), c(ncol(Uis), nrow(Uis))))
+  Y = (t(r1s) %*% M_A^2)/k
+  rA = colMeans(r1s)
   
-  mat = 1/4*rA^2*Uis^4 + rA*Y/2*Uis^2 + W^2*Uis^2 + Y^2/4 - W*Y*Uis - rA*W*Uis^3
+  mat = 1/4*rA^2*rowMeans(Uis^4) + rA*rowMeans(Y/2*Uis^2) + rowMeans(W^2*Uis^2 + Y^2/4 - W*Y*Uis) - rA*rowMeans(W*Uis^3)
   
-  return(rowMeans(mat)/n1)
-  
-}
-
-my_makeVars <- function(Uis, M_A){
-  
-  # stdized to ss = 1
-  
-  n1 = ncol(Uis)
-  
-  r1s = M_A %*% t(Uis) / (n - 1)
-  k = length(r1s)
-  
-  Uis = Uis
-  W = colMeans(M_A)
-  Y = (t(r1s) %*% M_A^2) / k
-  rA = mean(r1s)
-  
-  
-  Star2 <- 1/4*rA^2*Uis^4
-  Star3 <- rA*Y/2*Uis^2
-  Star1 <- W^2*Uis^2
-  Star4 <- Y^2/4
-  Dagger2 <- W*Y*Uis
-  Dagger1 <- rA*W*Uis^3
-  
-  mat <- Star1 + Star2 + Star3 + Star4 - Dagger1 - Dagger2
-  
-  return(rowMeans(mat)/n1)
+  return(mat/n1)
   
 }
 
@@ -55,9 +28,10 @@ makeVar <- function(Ui, M_A){
   
   r1s = M_A %*% Ui
   
-  Ui = Ui*sqrt(n1-1)
-  W = colMeans(M_A)*sqrt(n1-1)
-  Y = (t(r1s) %*% M_A^2)*(n1-1)/k
+  Ui = Ui*sqrt(n1)
+  M_A = M_A*sqrt(n1)
+  W = colMeans(M_A)
+  Y = (t(r1s) %*% M_A^2)/k
   rA = mean(r1s)
   
   mat = 1/4*rA^2*Ui^4 + rA*Y/2*Ui^2 + W^2*Ui^2 + Y^2/4 - W*Y*Ui - rA*W*Ui^3
--- varcalc_tests_uni.R
+++ varcalc_tests_uni.R
@@ -2,19 +2,24 @@ source("mvrnormR.R")
 library(microbenchmark)
 source("varcalcs.R")
 source("moment_calcs.R")
+source("makeVars.R")
+source("stdize.R")
 
 m <- 20
-rho <- 0.5
+rho <- 0
 nsims <- 1000
 ndata <- 1000
-Beta <- 1
+Beta <- 0
 s2 <- 1
 
 SigmaX_hom <- diag(1 - rho, m) + matrix(rep(rho, m^2), ncol = m)
 
 corsums <- vars <- rep(0, nsims)
+cormeans <- CM_vars <- rep(0, nsims)
 
 set.seed(12345)
+
+my_timers <- CM_timers <- rep(0, nsims)
 
 for (sim in 1:nsims) {
   
@@ -24,8 +29,17 @@ for (sim in 1:nsims) {
   Yvars <- Beta * rowSums(Data) + rnorm(ndata, sd = sqrt(s2))
   
   
-  # variance calculation
+  # variance calculations
   corsums[sim] <- sum(cor(Yvars, Data))
+  timer <- get_nanotime()
   vars[sim] <- varcalc1(Yvars, Data)
+  my_timers[sim] <- get_nanotime() - timer
+  cormeans[sim] <- mean(cor(Yvars, Data))
+  
+  timer <- get_nanotime()
+  Ui <- (Yvars - mean(Yvars)) / sqrt(sum(Yvars^2))
+  M_A <- stdize(t(Data))
+  CM_vars[sim] <- makeVar(Ui, M_A)
+  CM_timers[sim] <- get_nanotime() - timer
   
 }
