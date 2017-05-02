8826ce76c402d9737b6c9eda581713e408867210
jpalowitch <johnpalowitch@gmail.com>
2017-05-02 00:41:59 -0400

    added full mat calc

--- varcalcs.R
+++ varcalcs.R
@@ -77,3 +77,43 @@ varcalc1_multi <- function (Ymat, Xmat) {
   
   
 }
+
+varmat_uni <- function (Yvec, Xmat) {
+  
+  Xmat <- scale(Xmat)
+  Yvec <- as.vector(scale(Yvec))
+  
+  n <- nrow(Xmat)
+  
+  if (n != length(Yvec))
+    stop('X and Y variables must be of same dimension\n')
+  
+  # General calcs
+  xyCors <- as.vector(cor(Yvec, Xmat))
+  y4 <- sum(Yvec^4)
+  xRowSum <- rowSums(Xmat)
+  xRowSum2 <- tcrossprod(xyCors, Xmat^2)
+  
+  # star 1
+  S1 <- crossprod(Yvec * Xmat)
+  
+  # star 2
+  S2 <- y4 * tcrossprod(xyCors)
+  
+  # star 3
+  S3 <- tcrossprod(xyCors, xyCors * colSums(Xmat^2 * Yvec^2))
+  
+  # star 4
+  S4 <- tcrossprod(xyCors * t(Xmat^2))
+  
+  # dagger 1
+  D1 <- tcrossprod(xyCors, colSums(Xmat * Yvec^3))
+  
+  # dagger 2
+  D2 <- t(crossprod(Yvec * Xmat, Xmat^2)) * xyCors
+  
+  bigmat <- S1 + (S2 + S3 + t(S3) + S4) / 4 - (D1 + t(D1) + D2 + t(D2)) / 2
+  
+  return(sum(bigmat) / (n - 1))
+  
+}
