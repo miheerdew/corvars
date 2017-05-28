d265d498816e2a2f09c8a76d15a8d5ab75ce7b90
Miheer Dewaskar <miheerdew@gmail.com>
2017-05-28 13:05:14 -0400

    profiling code

--- profile_tracecalcs.R
+++ profile_tracecalcs.R
@@ -3,10 +3,10 @@ source("tracecalcs.R")
 
 set.seed(12345678)
 
-n <- 1000
+n <- 500
 nY <- 1000
 rho <- 0.5
-m <- 500
+m <- 50
 Beta <- 0
 s2 <- 1
 Sig <- diag(1 - rho, n) + matrix(rep(rho, n^2), ncol = n)
@@ -16,7 +16,7 @@ Sig <- diag(1 - rho, n) + matrix(rep(rho, n^2), ncol = n)
 X <- mvrnormR(m, rep(0, n), Sig)
 Y <- Beta * rowSums(X) + matrix(rnorm(m * nY, sd = sqrt(s2)), ncol = nY)
 
-show(profvis::profvis({
+p <- profvis::profvis({
   m <- nrow(X)
   XXt <- tcrossprod(X)
   XXt2 <- XXt^2
@@ -24,7 +24,10 @@ show(profvis::profvis({
   X3 <- X^3
   X4ColSums <- colSums(X^4)
   X2RowSums <- rowSums(X2)
-  do.call(rbind, lapply(1:nY, function(i) {
+  
+  res <- sapply(1:nY, function(i) 
     trace_large_x(Y[,i], X, m, XXt, XXt2, X2, X3, X4ColSums, X2RowSums)
-  }))
-}))
+    )
+})
+
+#show(p)
