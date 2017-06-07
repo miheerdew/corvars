b5509fecbf4b77041d9598503f315bec0c9894f7
Miheer Dewaskar <miheerdew@gmail.com>
2017-06-07 17:14:30 -0400

    Fixed bugs and looping

--- inf-pvals.R
+++ inf-pvals.R
@@ -1,16 +1,35 @@
 source("mvrnormR.R")
-source("bh_reject.R")
 library(doParallel)
 
-nsims <- 50
+nsims <- 100
 rho <- 0
 ndata <- 600
 alpha <- 0.05
+MAXIT <- 10
+
+bh_reject <- function (pvals, alpha, conserv = TRUE) {
+
+  m <- length(pvals)
+  
+  if (!conserv) {
+    pvals_adj <- m * pvals / rank(pvals)
+  } else {
+    mults <- sum(1 / c(1:m))
+    pvals_adj <- mults * m * pvals / rank(pvals)
+  }
+  
+  if (sum(pvals_adj <= alpha) > 0) {
+    thres <- max(pvals[pvals_adj <= alpha])
+    return(which(pvals <= thres))
+  } else {
+    return(integer(0))
+  }
+}
 
 sim <- function(ns) {
   foreach(n=ns, .combine = 'c') %:%
    foreach(sim=1:nsims,
-          .combine=function(...) mean(c(...)),
+          .combine=function(...) mean(c(...), na.rm=TRUE),
           .inorder = FALSE,
           .multicombine = TRUE) %dopar% {
      Sigma <- diag(n) #Completely independent case
@@ -21,34 +40,35 @@ sim <- function(ns) {
      pvals <- function(A){
        vars <- rep(length(A), n)
        vars[A] <-  length(A)-1
-       zs <- sqrt(ndata) * rowSums(R0[,A]) / sqrt(vars)
+       zs <- sqrt(ndata) * rowSums(R0[,A, drop=FALSE]) / sqrt(vars)
        pnorm(zs, lower.tail = FALSE)
      }
      
      max.pair <- as.vector(arrayInd(which.max(R0), .dim=dim(R0)))
      
-     A_old <- c()
-     A <- max.pair
-     #j <- 0
-     while (!setequal(A, A_old) && length(A) > 0){
-       A_old <- A
-       A <- bh_reject(pvals(A_old), alpha)
-     }
+      A_old <- c()
+      A <- max.pair
+      j <- 0
+      while (!setequal(A, A_old) && length(A) > 1 && j < MAXIT){
+        A_old <- A
+        A <- bh_reject(pvals(A_old), alpha)
+        j <- j + 1
+      }
      #cat(sprintf("Size of fixed point %d\n", length(A)))
-     length(A) > 0
+     ifelse(j >= MAXIT, NA, length(A) > 1)
   }
 }
 
 cl <- makePSOCKcluster(detectCores()-1)
 setDefaultCluster(cl)
-clusterExport(NULL, c('rho','ndata','mvrnormR','nsims', 'bh_reject', 'alpha'))
+clusterExport(NULL, c('rho','ndata','mvrnormR','nsims', 'bh_reject', 'alpha', 'MAXIT'))
 registerDoParallel(cl)
 
 RNGkind("L'Ecuyer-CMRG")
 set.seed(12347)
 
 
-x <- seq(100, 500, 100)#c(seq(100, 1000, by=100), seq(1000, 4000, by=500))
+x <- seq(100, 1000, by=100)
 y <- sim(x)
-plot(x, y, xlab="n", ylab="Probability of fixed point")
+#plot(x, y, xlab="n", ylab="Probability of fixed point")
 stopCluster(cl)
