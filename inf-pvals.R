7cd9f4e071145387dcee6be3c5e33dce926cbcb5
Miheer Dewaskar <miheerdew@gmail.com>
2017-06-07 15:58:01 -0400

    Type1 error counter example?

--- inf-pvals.R
+++ inf-pvals.R
@@ -1,16 +1,11 @@
 source("mvrnormR.R")
-source("varcalcs.R")
-library(parallel)
+source("bh_reject.R")
+library(doParallel)
 
-nsims <- 100
-rho <- 0.1
+nsims <- 50
+rho <- 0
 ndata <- 600
-
-
-pval <- function(y, X){
-  ndata <- nrow(X)
-  pnorm(sqrt(ndata) * sum(cor(y,X))/sqrt(varcalc1(y, X)), lower.tail = FALSE)
-}
+alpha <- 0.05
 
 sim <- function(ns) {
   foreach(n=ns, .combine = 'c') %:%
@@ -18,24 +13,42 @@ sim <- function(ns) {
           .combine=function(...) mean(c(...)),
           .inorder = FALSE,
           .multicombine = TRUE) %dopar% {
-     Sigma <- diag(1 - rho, n) + matrix(rep(rho, n^2), ncol = n)
+     Sigma <- diag(n) #Completely independent case
      X <- mvrnormR(ndata, rep(0, n), Sigma)
-     y <- rnorm(ndata)
-     r <- as.vector(crossprod(y, X)/ndata)
-     ids <- order(r, decreasing = TRUE)
-    pval(y, X[,ids[1], drop=FALSE])
+     R0 <- crossprod(X) / ndata
+     diag(R0) <- 0
+     
+     pvals <- function(A){
+       vars <- rep(length(A), n)
+       vars[A] <-  length(A)-1
+       zs <- sqrt(ndata) * rowSums(R0[,A]) / sqrt(vars)
+       pnorm(zs, lower.tail = FALSE)
+     }
+     
+     max.pair <- as.vector(arrayInd(which.max(R0), .dim=dim(R0)))
+     
+     A_old <- c()
+     A <- max.pair
+     #j <- 0
+     while (!setequal(A, A_old) && length(A) > 0){
+       A_old <- A
+       A <- bh_reject(pvals(A_old), alpha)
+     }
+     #cat(sprintf("Size of fixed point %d\n", length(A)))
+     length(A) > 0
   }
 }
 
 cl <- makePSOCKcluster(detectCores()-1)
 setDefaultCluster(cl)
-clusterExport(NULL, c('rho','ndata','mvrnormR','nsims', 'pval','varcalc1'))
+clusterExport(NULL, c('rho','ndata','mvrnormR','nsims', 'bh_reject', 'alpha'))
 registerDoParallel(cl)
 
 RNGkind("L'Ecuyer-CMRG")
 set.seed(12347)
 
-x <- c(seq(100, 900, by=100), seq(1000, 4000, by=500))
+
+x <- seq(100, 500, 100)#c(seq(100, 1000, by=100), seq(1000, 4000, by=500))
 y <- sim(x)
-plot(x, 1/y, xlab="n", ylab="1/min(p-value)")
+plot(x, y, xlab="n", ylab="Probability of fixed point")
 stopCluster(cl)
